8e16a94df70d336bf7423c216080900a179ef84c
multiversecoin <mnsmns2009@hotmail.com>
2026-09-12 14:39:42 -0300

    Fix logo paths for proper loading

--- src/components/MultiverseLogo.tsx
+++ src/components/MultiverseLogo.tsx
@@ -4,7 +4,7 @@ export default function MultiverseLogo({ size = 60 }: { size?: number }) {
   return (
     <img
       src="/logo.jpg"
-      alt="Multiverse Logo"
+      alt="Multiverse Coin Logo"
       style={{ width: size, height: size }}
       className="mx-auto object-contain"
     />
